df93efd52171f90836b4cd3e4a06a4fe088e0335
Kamil Wolszczak <wolszczak.kamil@gmail.com>
2023-04-25 13:03:37 +0200

    second option to print Lego.class instances

--- src/main/java/com/kwolszczak/lego/App.java
+++ src/main/java/com/kwolszczak/lego/App.java
@@ -1,7 +1,41 @@
 package com.kwolszczak.lego;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        /**
+         * First idea
+         *
+         * */
+        var starWarsModel = new Lego("Star Wars", 100);
+        var harryPotterModel = new Lego("Harry Potter", 500);
+        var winnieThePoohModel = new Lego("Winnie the Pooh", 2_000);
+
+        List<Lego> legoList = new ArrayList<>();
+        legoList.add(starWarsModel);
+        legoList.add(harryPotterModel);
+        legoList.add(winnieThePoohModel);
+
+        legoList.stream().filter(ob -> ob.getPrice() < 1_000).forEach(System.out::println);
+
+
+        /**
+         * Second idea
+         *
+         * */
+        Lego[] arrayLego = new Lego[]{
+                new Lego("Star Wars", 100),
+                new Lego("Harry Potter", 500),
+                new Lego("Winnie the Pooh", 2_000)
+        };
+
+        for (Lego model : arrayLego) {
+            //var below1000 = model.getPrice()<1000 ? model : null;
+            //System.out.println(below1000);
+            if (model.getPrice() < 1000) System.out.println(model);
+        }
 
     }
 }
--- src/main/java/com/kwolszczak/lego/Lego.java
+++ src/main/java/com/kwolszczak/lego/Lego.java
@@ -9,10 +9,43 @@ public class Lego {
     Lego(double price) {
         this("Test", price,new String[1] );
     }
+    Lego(String name, double price){
+        this.name=name;
+        this.price= price;
+    }
 
     Lego(String name, double price, String[] pieces) {
         this.name = name;
         this.price = price;
         this.pieces = pieces;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String[] getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(String[] pieces) {
+        this.pieces = pieces;
+    }
+
+    @Override
+    public String toString(){
+        return "Lego set:"+name+". Price: "+price+" PLN";
     }
 }
