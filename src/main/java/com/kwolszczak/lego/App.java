01de6d2fb841c20d3f4a709643e24c8be524cfe6
Kamil Wolszczak <wolszczak.kamil@gmail.com>
2023-05-10 22:28:41 +0200

    removed unnecessary getters/setter constructors from Lego.class

--- src/main/java/com/kwolszczak/lego/App.java
+++ src/main/java/com/kwolszczak/lego/App.java
@@ -1,41 +1,35 @@
 package com.kwolszczak.lego;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class App {
 
     private static final int ACCEPTABLE_PRICE = 1500;
     public static void main(String[] args) {
-
         /**
          * First option
          *
          * */
-        var starWarsModel = new Lego("Star Wars", 100);
-        var harryPotterModel = new Lego("Harry Potter", 500);
-        var winnieThePoohModel = new Lego("Winnie the Pooh", 2_000);
+        var starWarsModel = new Lego("Star Wars", 100,50);
+        var harryPotterModel = new Lego("Harry Potter", 500,460);
+        var winnieThePoohModel = new Lego("Winnie the Pooh", 2_000,130);
 
-        List<Lego> legoModelPack = new ArrayList<>();
-        legoModelPack.add(starWarsModel);
-        legoModelPack.add(harryPotterModel);
-        legoModelPack.add(winnieThePoohModel);
-
-        legoModelPack.stream().filter(model -> model.getPrice() < ACCEPTABLE_PRICE).forEach(System.out::println);
+        Arrays.asList(starWarsModel,harryPotterModel,winnieThePoohModel)
+                .stream()
+                .filter(model -> model.getPrice() < ACCEPTABLE_PRICE)
+                .forEach(System.out::println);
 
         /**
          * Second option
          *
          * */
         Lego[] legoModelCollection = new Lego[]{
-                new Lego("Star Wars", 100),
-                new Lego("Harry Potter", 500),
-                new Lego("Winnie the Pooh", 2_000)
+                new Lego("Star Wars", 100,50),
+                new Lego("Harry Potter", 500,460),
+                new Lego("Winnie the Pooh", 2_000,130)
         };
 
         for (Lego model : legoModelCollection) {
-            /*var acceptableModel = model.getPrice()<acceptablePrice ? model : null;
-            System.out.println(acceptablePrice);*/
             if (model.getPrice() < ACCEPTABLE_PRICE) System.out.println(model);
         }
 
--- src/main/java/com/kwolszczak/lego/Lego.java
+++ src/main/java/com/kwolszczak/lego/Lego.java
@@ -4,48 +4,21 @@ public class Lego {
 
     private String name;
     private double price;
-    private String[] pieces;
+    private int numberOfPieces;
 
-    Lego(double price) {
-        this("Test", price,new String[1] );
-    }
-    Lego(String name, double price){
-        this.name=name;
-        this.price= price;
-    }
 
-    Lego(String name, double price, String[] pieces) {
+    Lego(String name, double price, int numberOfPieces) {
         this.name = name;
         this.price = price;
-        this.pieces = pieces;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.numberOfPieces = numberOfPieces;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String[] getPieces() {
-        return pieces;
-    }
-
-    public void setPieces(String[] pieces) {
-        this.pieces = pieces;
-    }
-
     @Override
     public String toString(){
-        return "Lego set:"+name+". Price: "+price+" PLN";
+        return "Lego set:"+name+". Price: "+price+" PLN"+". Number of pieces: "+numberOfPieces;
     }
 }
